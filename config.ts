15abe930e338218a3793b95d0a181e96cd447d7c
Beary <84789008+0xbeary@users.noreply.github.com>
2025-08-28 17:35:19 +0000

    simplified the config

--- config.ts
+++ config.ts
@@ -2,10 +2,9 @@ import 'dotenv/config';
 
 export function getConfig() {
   return {
-    network: process.env.NETWORK || 'mainnet',
-    dbPath: process.env.DB_PATH || './db',
+    network: 'mainnet',
     blockFrom: parseInt(process.env.BLOCK_FROM || '0'),
-    contractAddress: process.env.CONTRACT_ADDRESS || '0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72',
+    contractAddress: '0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72',
     portal: {
       url: process.env.PORTAL_URL || 'https://portal.sqd.dev/datasets/ethereum-mainnet',
     },
